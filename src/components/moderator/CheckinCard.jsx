df83bbdb61c8d2e157bb9e4c67ecaf448b0dd26c
trungmac07 <mttrung16@gmail.com>
2023-07-22 19:55:46 +0700

    update ui mode thêm checkin checkout screen

---
+++ src/components/moderator/CheckinCard.jsx
@@ -0,0 +1,90 @@
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
+
+
+import React from 'react'
+
+export default function CheckinCard({ Username, PhoneNumber, Room, CheckInDate }) {
+    return (
+
+        <View style={styles.card}>
+
+            <View style={{ flexDirection: 'row', padding: 10, marginTop: 7 }}>
+                <Image style={{ flex: 3.5, height: 'auto', minHeight: 170, borderRadius: 15, borderWidth: 1, }} source={require('../../assets/images/demoHotel.jpg')} />
+                <View style={{ flex: 5, }}>
+                    <Text style={{ marginLeft: 10, justifyContent: 'center', marginTop: 0, fontSize: 17, fontWeight: 'bold', color: '#FF6400' }}> {Room} </Text>
+
+                    
+                    <View style={{ flexDirection: 'row', marginTop: 15 }}>
+                        <Image style={styles.icon} source={require('../../assets/icons/checkin.png')} />
+                        <Text style={{ marginLeft: 10, justifyContent: 'center', marginTop: 5, fontSize: 15, fontWeight: 'bold' }}> {CheckInDate} </Text>
+                    </View>
+
+                    <View style={{ flexDirection: 'row', marginTop: 5 }}>
+                        <Image style={styles.icon} source={require('../../assets/icons/phone.png')} />
+                        <Text style={{ marginLeft: 10, justifyContent: 'center', marginTop: 5, fontSize: 15, fontWeight: 'bold' }}> {PhoneNumber} </Text>
+                    </View>
+
+                    <View style={{ flexDirection: 'row', marginTop: 5, height:50 }}>
+                        <Image style={styles.icon} source={require('../../assets/icons/people_black.png')} />
+                        <Text style={{ marginLeft: 10, justifyContent: 'center', marginTop: 5, fontSize: 15, fontWeight: 'bold' }}> {shortUsername(Username)} 
+                        </Text>
+                    </View>
+                    
+                    
+
+                    <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                        <TouchableOpacity style={{ height: 40, flex: 1, borderRadius: 20, marginLeft: 10, backgroundColor: '#FF6400' }}>
+                            <Text style={{ textAlign: 'center', paddingTop: 10, color: 'white', fontWeight: 'bold' }}> {"Check In"} </Text>
+                        </TouchableOpacity>
+
+                    </View>
+                </View>
+            </View>
+
+        </View>
+
+    )
+}
+
+
+const shortUsername = (Username) => {
+    if(Username.length > 25)
+        return "..." + Username.substr(Username.length-25);
+    else
+        return Username
+    }
+
+const styles = StyleSheet.create({
+    card:
+    {
+
+        height: 'auto',
+        minHeight: 200,
+        backgroundColor: 'white',
+        padding: 10,
+        marginTop: 15,
+        borderRadius: 15,
+
+    },
+
+    icon:
+    {
+        height: 22,
+        width: 22,
+        marginLeft: 10,
+        marginTop: 5,
+        maxHeight: 22,
+        maxWidth: 22,
+
+    },
+
+    date:
+    {
+        marginLeft: 10,
+        justifyContent: 'center',
+        marginTop: 0,
+        fontSize: 12,
+        color: '#888888'
+    }
+
+});
--- src/screens/Moderator/MainScreen.jsx
+++ src/screens/Moderator/MainScreen.jsx
@@ -1,67 +1,106 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from "react-native";
 import TopBar from "../../components/moderator/TopBar";
 import SearchBar from "../../components/moderator/SearchBar";
 import VerifyWaitingCard from "../../components/moderator/VerifyWaitingCard";
-
+import CheckinCard from "../../components/moderator/CheckinCard";
+import CheckoutCard from "../../components/moderator/CheckoutCard";
 import generalStyles from "../../styles";
+
 export default function MainScreen() {
 
-    const [page,setPage] = useState('verify')
-    let verifyList = []
+    const [page, setPage] = useState('verify')
+    const [verifyColor, setVerifyColor] = useState('#FF6400')
+    const [checkinColor, setCheckinColor] = useState('#888888')
+    const [checkoutColor, setCheckoutColor] = useState('#888888')
+
+    
+
+    const setButtonColor = page => {
+        setVerifyColor('#888888')
+        setCheckinColor('#888888')
+        setCheckoutColor('#888888')
+
+        if(page === 'verify') 
+            setVerifyColor('#FF6400')
+        else if(page === 'checkin') 
+            setCheckinColor('#FF6400')
+        else 
+            setCheckoutColor('#FF6400')
+    }
+
     return (
         <View style={generalStyles.page_container}>
-            <TopBar Title={"Hotel Name"}/>
-            <SearchBar/>
-            <View style = {{flexDirection : 'row', marginBottom:10}}>
-                <TouchableOpacity style={{height: 40, flex:1 ,borderWidth: 1, borderRadius: 20, marginTop:15, marginRight:10}}
-                    onPress={() => {setPage('verify')}}>
-                    <Text style={{textAlign:'center', paddingTop:7}}> Verify </Text>
+            <TopBar Title={"Hotel Name"} />
+            <SearchBar />
+            <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+                <TouchableOpacity style={{backgroundColor: verifyColor, height: 40, flex: 1,  borderRadius: 20, marginTop: 15, marginRight: 10 }}
+                    onPress={() => { setPage('verify'); setButtonColor('verify'); }}>
+                    <Text style={{ textAlign: 'center', paddingTop: 10, color: 'white', fontWeight:'bold' }}> Verify </Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={{height: 40, flex:1 ,borderWidth: 1, borderRadius: 20, marginTop:15, margin:0}} 
-                    onPress={() => {setPage('checkin')}}>
-                    <Text style={{textAlign:'center', paddingTop:7}}> Check In </Text>
+                <TouchableOpacity style={{backgroundColor: checkinColor, height: 40, flex: 1,  borderRadius: 20, marginTop: 15, margin: 0 }}
+                    onPress={() => { setPage('checkin'); setButtonColor('checkin'); }}>
+                    <Text style={{ textAlign: 'center', paddingTop: 10, color:'white', fontWeight:'bold' }}> Check In </Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={{height: 40, flex:1 ,borderWidth: 1, borderRadius: 20, marginTop:15, marginLeft:10}}
-                    onPress={() => {setPage('checkout')}}>
-                    <Text style={{textAlign:'center', paddingTop:7}}> Check Out </Text>
+                <TouchableOpacity style={{backgroundColor: checkoutColor, height: 40, flex: 1,  borderRadius: 20, marginTop: 15, marginLeft: 10 }}
+                    onPress={() => { setPage('checkout'); setButtonColor('checkout'); }}>
+                    <Text style={{ textAlign: 'center', paddingTop: 10, color: 'white', fontWeight:'bold' }}> Check Out </Text>
                 </TouchableOpacity>
             </View>
 
             {
 
-                page === 'verify' && (
+                (page === 'verify') && (
                     <ScrollView>
-                        <Text style= {{ marginTop:20, fontSize : 25, fontWeight:'bold'}}>Waiting for verifying</Text>
-                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room = 'Phong Tinh Iu Chi Anh Va Em <3' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'}/>
-                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room = 'Phong Tinh Iu Chi Anh Va Em <3' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'}/>
-                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room = 'Phong Tinh Iu Chi Anh Va Em <3' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'}/>
-                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room = 'Phong Tinh Iu Chi Anh Va Em <3' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'}/>
-                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room = 'Phong Tinh Iu Chi Anh Va Em <3' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'}/>
-                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room = 'Phong Tinh Iu Chi Anh Va Em <3' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'}/>
-                    
-                    
-                    
+                        <Text style={{ marginTop: 20, fontSize: 25, fontWeight: 'bold' }}>Waiting for verifying</Text>
+                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <VerifyWaitingCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+
                     </ScrollView>
-                    
+
+                ) 
+            }
+            {
+                page === 'checkin' && (
+                    <ScrollView>
+                        <Text style={{ marginTop: 20, fontSize: 25, fontWeight: 'bold' }}>Check in</Text>
+                        <CheckinCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckinCard Username={'Tran Thu Minh Minh Minh Minh Nguyen Thi Cong Khanh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckinCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckinCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckinCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckinCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+
+                    </ScrollView>
                 )
             }
             {
-                page === 'checkin' &&  (
-                    <Text style= {{ marginTop:20, fontSize : 25, fontWeight:'bold'}}>Waiting for checking in</Text>
+                page === 'checkout' && (
+                    <ScrollView>
+                        <Text style={{ marginTop: 20, fontSize: 25, fontWeight: 'bold' }}>Check out</Text>
+                        <CheckoutCard Username={'Tran Thu Minh Minh Minh Minh Nguyen Thi Cong Khanh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckoutCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckoutCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckoutCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckoutCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+                        <CheckoutCard Username={'Tran Thu Minh'} PhoneNumber={'0909996669'} Room='Phong khach san' NumOfRoom={5} CheckInDate={'15/07/2023'} CheckOutDate={'17/07/2023'} />
+
+                    </ScrollView>
                 )
             }
-            {
-                page === 'checkout' &&  (
-                    <Text style= {{ marginTop:20, fontSize : 25, fontWeight:'bold'}}>Waiting for checking out</Text>
-                )
-            }
-            
+
         </View>
 
-        
+
 
     );
 }
+
+
+
 
 const styles = StyleSheet.create({});
