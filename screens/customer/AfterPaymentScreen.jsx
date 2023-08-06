17ee1e2e7eda4c4d89dc8eddd5418a76730d9199
Thế Nghĩa <nghiask2018@gmail.com>
2023-08-06 17:12:22 +0700

    Polishing Customer AfterPayment Screen

--- screens/customer/AfterPaymentScreen.jsx
+++ screens/customer/AfterPaymentScreen.jsx
@@ -1,106 +1,149 @@
 import { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View, ImageBackground, ScrollView } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView } from "react-native";
+import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
 
 // Import Style & Theme
 import { COLORS, TEXTS } from '../../constants/theme'
 import customerStyles from '../../styles/customer'
 
 export default function AfterPaymentScreen({ navigation, route, parentSlug }) {
-    // console.log(route.params)
+    console.log('[Customer] AfterPaymentScreen')
 
     return (
-        <View style={{flex: 1}}>
-            <ScrollView style={{...customerStyles.page_container , marginBottom: 80}}>
+        <View style={customerStyles.page_container}>
+            <ScrollView style={{flex: 1}}>
                 {/* Top Bar */}
-                <View style={styles.top_bar}>
-                    <TouchableOpacity
-                        style={{ width: 62, padding: 10, backgroundColor: '#fff', borderRadius: 12, marginLeft: 12 }}
-                        onPress={() => navigation.navigate('HotelPage', { slug: route.params.parentSlug })}
-                    >
-                        <Text style={{ color: COLORS.primary, fontWeight: '600' }}>
-                            Back
-                        </Text>
-                    </TouchableOpacity>
-                    <Text style={customerStyles.page_name}>Payment Successfully</Text>
+                <View style={customerStyles.top_bar}>
+                    <Text style={customerStyles.top_bar_title}>Booking Status</Text>
                 </View>
 
 
 
-                {/* Thumbnail Slider */}
+                {/* Illustration */}
+                <View style={styles.illustration_wrapper}>
+                    <Image
+                        source={require('../../assets/customer/booking_successfully.png')}
+                        style={styles.illustration_image}
+                    />
+                </View>
+
+                {/* Illustration */}
+                <View style={styles.icon_wrapper}>
+                    <Image
+                        source={require('../../assets/customer/notification_tick.png')}
+                        style={styles.icon_image}
+                    />
+                </View>
+
+                {/* Booking Status */}
                 <View style={customerStyles.section_container}>
-                    <ImageBackground
-                        source={require('../../assets/customer/demo.jpg')}
-                        resizeMode="cover"
-                        style={{ ...styles.thumbnail_slider, justifyContent: 'flex-start' }}
-                    >
-                    </ImageBackground>
+                    <Text style={styles.text_1}>Booking successfully</Text>
+                    <Text style={styles.text_2}>Congratulations on your successful booking at</Text>
+                    <Text style={styles.hotel_name}>Haley House</Text>
                 </View>
-
-
-                {/* Room Information */}
-                <View style={customerStyles.section_container}>
-                    <Text style={customerStyles.heading_1}>Haley House</Text>
-                    <Text style={customerStyles.subheading_1}>Deluxe Room</Text>
-                    <Text style={customerStyles.text_1}>District 7, HCM</Text>
-                </View>
-
-                <View style={customerStyles.divider}></View>
-
-
-
-
             </ScrollView>
-            <View style={styles.bar_container}>
-                <View style={{flex:1, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 28}}>
-                    <TouchableOpacity
-                        style={styles.bar_button}
-                        onPress={() => {navigation.navigate('MainPage')}}>
-                        <Text style={customerStyles.button_text_1}>Back to Home</Text>
-                    </TouchableOpacity>
-                </View>
+
+            <View style={styles.fixed_bar_container}>
+                <TouchableOpacity
+                    style={styles.check_button}
+                    onPress={() => { navigation.navigate('ReservationPage') }}>
+                    <Text style={styles.check_button_text}>Check Reservation</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.home_button}
+                    onPress={() => { navigation.navigate('MainPage') }}>
+                    <Text style={styles.home_button_text}>Back to Home Page</Text>
+                </TouchableOpacity>
             </View>
+
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    top_bar: {
-        height: 60,
+    illustration_wrapper: {
+        alignSelf: 'center',
+        width: 400,
+        height: 400,
+    },
+    illustration_image: {
         width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
+        height: '100%',
+        resizeMode: 'contain',
     },
 
-    thumbnail_slider: {
-        marginTop: 12,
+    icon_wrapper: {
+        marginTop: -30,
+        alignSelf: 'center',
+        width: 100,
+        height: 50,
+    },
+    icon_image: {
         width: '100%',
-        height: 210,
-        borderRadius: 20,
-        overflow: 'hidden',
+        height: '100%',
+        resizeMode: 'contain',
     },
 
-    bar_container: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
-        backgroundColor: '#fff',
-        height: 80,
-        elevation: 20,
-    },
-    bar_price: {
+    text_1 : {
         color: COLORS.heading_text,
         fontSize: TEXTS.xxl,
         fontWeight: '600',
-        alignSelf: 'center'
+        textAlign: 'center',
     },
-    bar_button: {
+    text_2 : {
+        color: COLORS.subheading_text,
+        fontSize: TEXTS.sm,
+        fontWeight: '500',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    hotel_name : {
+        color: COLORS.heading_text,
+        fontSize: TEXTS.lg,
+        fontWeight: '600',
+        textAlign: 'center',
+        marginTop: 4,
+    },
+
+    check_button: {
         backgroundColor: COLORS.primary,
-        height: 52,
+        height: 56,
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 50,
         width: '100%',
     },
+    check_button_text: {
+        color: '#fff',
+        fontWeight: '800',
+        fontSize: TEXTS.lg
+    },
 
+    home_button: {
+        marginTop: 16,
+        backgroundColor: '#CCC',
+        height: 56,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        width: '100%',
+    },
+    home_button_text: {
+        color: '#fff',
+        fontWeight: '800',
+        fontSize: TEXTS.lg
+    },
+
+    fixed_bar_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: '#fff',
+        height: 160,
+        elevation: 20,
+        paddingHorizontal: 24,
+        paddingTop: 12,
+    },
 });
