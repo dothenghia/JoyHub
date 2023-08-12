cf6bcbcbf74d48fa58707f8b2514ace42a0c7e47
trungmac07 <mttrung16@gmail.com>
2023-08-12 20:01:11 +0700

    Mod Notification model

--- models/moderator/getNotifications.js
+++ models/moderator/getNotifications.js
@@ -1,7 +1,71 @@
 
-
+const notifications = [
+    {
+        id: 'ID1234',
+        title:'Bao\' Dong^. Do?, Anh Nho\' Em',
+        content: 'Bao\' Dong^. Do?, Anh Nho\' Em',
+        date: '21/12/2022',
+        type: 'warning'
+    },
+    {
+        id: 'ID1234',
+        title:'[GLOBAL BAN] ZEROS,VINBOI,DIA1,CANHCUT,DNK,NPER',
+        content: 'ZEROS, VINBOI, DIA1,CANHCUT,DNK,NPER',
+        date: '11/08/2023',
+        type: 'approved'
+    },
+    {
+        id: 'ID1234',
+        title:'[KHACH DA HUY PHONG] : NGOC TRINH 8X',
+        content: 'KHACH: NGOC TRINH 8X DA HUY PHONG',
+        date: '21/01/2023',
+        type: 'approved'
+    },
+    {
+        id: 'ID1234',
+        title:'Bao\' Dong^. Do?, Anh Nho\' Em',
+        content: 'Bao\' Dong^. Do?, Anh Nho\' Em',
+        date: '21/12/2022',
+        type: 'warning'
+    },
+    {
+        id: 'ID1234',
+        title:'[GLOBAL BAN]',
+        content: 'ZEROS,VINBOI,DIA1,CANHCUT,DNK,NPER',
+        date: '11/08/2023',
+        type: 'warning'
+    },
+    {
+        id: 'ID1234',
+        title:'[KHACH DA HUY PHONG]',
+        content: 'KHACH: NGOC TRINH 8X DA HUY PHONG',
+        date: '21/01/2023',
+        type: 'warning'
+    },
+    {
+        id: 'ID1234',
+        title:'[DAY LA THONG BAO]',
+        content: 'Bao\' Dong^. Do?, Anh Nho\' Em',
+        date: '21/12/2022',
+        type: 'approved'
+    },
+    {
+        id: 'ID1234',
+        title:'[GLOBAL BAN]',
+        content: 'ZEROS,VINBOI,DIA1,CANHCUT,DNK,NPER',
+        date: '11/08/2023',
+        type: 'warning'
+    },
+    {
+        id: 'ID1234',
+        title:'[KHACH DA HUY PHONG]',
+        content: 'KHACH: NGOC TRINH 8X DA HUY PHONG',
+        date: '21/01/2023',
+        type: 'approved'
+    },
+]
 
 export default async function getNotifications()
 {
-    
+    return notifications
 }
