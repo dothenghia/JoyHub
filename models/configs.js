4594e0e4efece8d2d4627beabc393957186eac45
Khanh Nguyen <Kn11072003a@gmail.com>
2023-08-24 14:41:24 +0700

    Add report for admin

--- models/configs.js
+++ models/configs.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    //baseURL: "https://timely-chebakia-b0eb91.netlify.app/.netlify/functions/api",
-    baseURL: "https://409c-115-73-217-121.ngrok-free.app/.netlify/functions/api",
+    // baseURL: "https://timely-chebakia-b0eb91.netlify.app/.netlify/functions/api",
+    baseURL: "https://2237-115-73-214-48.ngrok-free.app/.netlify/functions/api",
 });
 
 export default axiosInstance;
